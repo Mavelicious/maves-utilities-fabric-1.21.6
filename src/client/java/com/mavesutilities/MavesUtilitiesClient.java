3563f528fc0d8ab14c0c37e10891c7aa1191a783
Mavelicious <marcusvcalho@gmail.com>
2025-01-08 18:46:52 -0300

    Initial commit

---
+++ src/client/java/com/mavesutilities/MavesUtilitiesClient.java
@@ -0,0 +1,10 @@
+package com.mavesutilities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MavesUtilitiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/mavesutilities/MavesUtilitiesMod.java
@@ -0,0 +1,18 @@
+package com.mavesutilities;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MavesUtilitiesMod implements ModInitializer {
+
+	public static final String MOD_ID = "mavesutilities";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
